a3c5387e102c4d36fc12f230a408bb100d199aef
Alex Bomtempo <santosbomtempo@gmail.com>
2024-02-22 12:07:45 -0300

    Subindo alteração de rota de endereço a ser testado

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,7 +2,7 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
 
     beforeEach(() => {
-        cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        cy.visit('.src/index.html')
     });
     it('verifica o título da aplicação', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
